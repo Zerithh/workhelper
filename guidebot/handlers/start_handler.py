5e7d2af7f8763a0c0f2e3470a82ca62ea78706cd
Zerithh <shostak.business@gmail.com>
2025-10-18 23:03:01 +0200

    first commit

--- guidebot/handlers/start_handler.py
+++
@@ -1,6 +0,0 @@
-from keyboards.start_kb import start_kb
-
-def register(bot):
-    @bot.message_handler(commands=['start'])
-    def handle_start(message):
-        bot.send_message(message.chat.id, 'Привіт!\nРозпочнемо роботу.\nОбери тур зі списку або створи новий.', reply_markup=start_kb())
--- guidebot/keyboards/start_kb.py
+++
@@ -1,9 +0,0 @@
-from telebot import types
-
-def start_kb():
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    markup.row(
-        types.KeyboardButton('Обрати тур зі списку'),
-        types.KeyboardButton('Створити новий тур')
-    )
-    return markup
